969fd164ca7891db800ca810d5fbe2254b1357c3
Sudarshan <mrsudarshank8124@gmail.com>
2026-05-27 22:26:38 +0530

    feat: UI updates

--- src/components/PostHogProvider.tsx
+++ src/components/PostHogProvider.tsx
@@ -25,7 +25,8 @@ function PostHogPageView() {
 export default function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      api_host: '/ingest',
+      ui_host: 'https://us.posthog.com',
       person_profiles: 'identified_only',
       capture_pageview: false,
       capture_pageleave: true,
